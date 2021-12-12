77a2860065c7ff72483d545ec032768c8fe4467a
root <dnlwrn@outlook.com>
2021-12-12 22:16:37 +0000

    revert: js changes

--- index.js
+++ index.js
@@ -29,39 +29,13 @@ window.onload = function getDate() {
         document.getElementById("launchday").innerText = String(daysDifference);
     }
 
-
-    var viewPortWidth;
-    var viewPortHeight;
-    
-    // the more standards compliant browsers (mozilla/netscape/opera/IE7) use window.innerWidth and window.innerHeight
-    if (typeof window.innerWidth != 'undefined') {
-        viewPortWidth = window.innerWidth,
-        viewPortHeight = window.innerHeight
-    }
-    
-    // IE6 in standards compliant mode (i.e. with a valid doctype as the first line in the document)
-    else if (typeof document.documentElement != 'undefined'
-    && typeof document.documentElement.clientWidth !=
-    'undefined' && document.documentElement.clientWidth != 0) {
-        viewPortWidth = document.documentElement.clientWidth,
-        viewPortHeight = document.documentElement.clientHeight
-    }
-    
-    // older versions of IE
-    else {
-        viewPortWidth = document.getElementsByTagName('body')[0].clientWidth,
-        viewPortHeight = document.getElementsByTagName('body')[0].clientHeight
-    }
-
 	//canvas init
 	var canvas = document.getElementById("canvas");
 	var ctx = canvas.getContext("2d");
 	
 	//canvas dimensions
-	var W = viewPortWidth;
-    console.log("width:" + W);
-	var H = viewPortHeight;
-    console.log("height:" + H);
+	var W = window.innerWidth;
+	var H = window.innerHeight;
 	canvas.width = W;
 	canvas.height = H;
     
